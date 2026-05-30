c6a77015f0c944a838484501e6356ee49f7cf6ed
thandra2809 <thandra82@gmail.com>
2026-05-30 12:10:40 -0500

    added changes

--- src/main/java/org/namastedev/twopointersandslidingwindow/FourSumLC18.java
+++ src/main/java/org/namastedev/twopointersandslidingwindow/FourSumLC18.java
@@ -1,7 +1,40 @@
 package org.namastedev.twopointersandslidingwindow;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FourSumLC18 {
     static void main() {
+        int[] nums = new int[]{1,0,-1,0,-2,2};     // {-1, 0, 1, 2, -1, -4};
+        int target = 0;
 
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        int n = nums.length;
+
+        for (int i = 0; i < n-3; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            for (int j = i+1; j < n-2; j++) {
+                if (j > i+1 && nums[j] == nums[j-1]) continue;
+                int left = j+1, right = n-1;
+                while (left < right) {
+                    long sum = (long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                        while (left<right && nums[left]==nums[left+1]) left++;
+                        while (left<right && nums[right]==nums[right-1]) right--;
+                        left++; right--;
+                    } else if (sum < target) left++;
+                    else right--;
+                }
+            }
+        }
+        // return result;
+
+        System.out.println(result);
     }
+
+
 }
